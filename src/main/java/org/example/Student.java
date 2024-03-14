5899388ec767656786cd46bd23a2e9d85d2d3eba
ms_father <sahilbansal5542@gmail.com>
2024-03-15 01:39:13 +0530

    added sail bansal file

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.security.PublicKey;
+
+public class Student {
+    public String name;
+    public int age;
+
+}
---
+++ src/main/java/org/example/bansal.java
@@ -0,0 +1,8 @@
+package org.example;
+
+import java.util.Properties;
+
+public class bansal {
+    public int x;
+    public int y;
+}
