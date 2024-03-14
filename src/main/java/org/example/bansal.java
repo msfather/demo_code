6bf5efab6a2bd6054a5bae2d3d080710dbad7c23
ms_father <sahilbansal5542@gmail.com>
2024-03-15 01:46:23 +0530

    sandeep added

--- src/main/java/org/example/bansal.java
+++ src/main/java/org/example/bansal.java
@@ -3,6 +3,21 @@ package org.example;
 import java.util.Properties;
 
 public class bansal {
-    public int x;
-    public int y;
+    private int x;
+    private int y;
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
---
+++ src/main/java/org/example/sandeep.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class sandeep {
+    public String s;
+
+}
